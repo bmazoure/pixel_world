Fixed rewards (off by one error for states)

pixel_world/envs/pixel_world.py
@@ -154,12 +154,14 @@ class PixelWorld(gym.Env):
             action = self._action2vec(action)
         else:
             action = action + np.array([0.5,0.5])
+        is_terminal = int(self.current_state.terminal)
         s_p_a = self.current_state.coords + action
         s_p_a, next_state = self._project(s_p_a)
-        self.current_state = self.current_state if next_state == -1 else next_state
         reward = self.current_state.get_reward()
+        self.current_state = self.current_state if next_state == -1 else next_state
+        
         next_obs = s_p_a if not self.as_image else self._map2screen(True)
-        return next_obs,reward,int(self.current_state.terminal),{} 
+        return next_obs,reward,is_terminal,{} 
     
     def reset(self):
         self.current_state = self.initial_state